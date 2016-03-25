be0ca3f133a336d76f823f4c921ef16a51d5795c
Erik van der Hoeven <Erik.Hoeven@gmail.com>
2016-03-25 20:54:20 +0100

    findAndUpdate JSON documents in MongoDB. FindAPI als voor AJAX handling.

--- views/BusinessRules/index.js
+++ views/BusinessRules/index.js
@@ -95,13 +95,131 @@ exports.create = function(req, res, next) {
            // req.flash('success','BusinessRule Added');
             res.location('/BusinessRules');
             res.redirect('/BusinessRules');
+
         });
     });
     workflow.emit('validate');
 };
 
-exports.findApiData = function (req, res, next) {
-    console.log("findApiData");
-    console.log(req.body);
+exports.findApiData = function(req, res, next) {
+    console.log('Start findAPIData: ');
+    console.info(req.body._id);
+    console.info(req.body.updateField);
+    console.info(req.body.updateValue);
+    var MongoClient = require('mongodb').MongoClient;
+    var assert = require('assert');
+    var ObjectId = require('mongodb').ObjectID;
+    var url = 'mongodb://localhost:27017/commevents';
+    var businesRule = {};
 
-};
+
+    //function updateBusinessRule(updateField, updateValue){
+    var lookupValue;
+    var typeBusinessRule;
+    var tagCattegory;
+    var creationDate;
+    var lastUpdatedate;
+    var tagScore;
+
+    function setBusinessRule(doc){
+
+        lookupValue = "";
+        typeBusinessRule = "";
+        tagCattegory = "";
+        creationDate = Date();
+        tagScore = "";
+        lastUpdatedate =  Date();
+
+        // (1)Check if the form values ar not empty
+        // (1.a) If form value is empty and document(doc) value is not empty replace it with the doc value
+        // (1.b) If form value and doc value is empty return empty string
+
+        console.log("Zoeken op:" + req.body.updateField);
+
+        // lookupValue
+        if (req.body.updateField == "opzoekwaarde"){
+            lookupValue = req.body.updateValue;
+
+            if (doc.typeBusinessRule != null) { typeBusinessRule = doc.typeBusinessRule; }
+            if (doc.tagCattegory != null) { tagCattegory = doc.tagCattegory; }
+            if (doc.tagScore != null) { tagScore = doc.tagScore; }
+            if (doc.creationDate != null) { creationDate = doc.creationDate; }
+        }
+        // tagCattegory
+        if (req.body.updateField == "cattegorie"){
+            tagCattegory = req.body.updateValue;
+
+            if (doc.typeBusinessRule != null) { typeBusinessRule = doc.typeBusinessRule; }
+            if (doc.lookupValue != null) { lookupValue = doc.lookupValue; }
+            if (doc.tagScore != null) { tagScore = doc.tagScore; }
+            if (doc.creationDate != null) { creationDate = doc.creationDate; }
+        }
+
+        // typeBusinessRule
+        if (req.body.updateField == "typeBusinessRule"){
+            console.log(" Is ..... typeBusinessRule. De waarde wordt: " + req.body.updateValue  );
+            typeBusinessRule = req.body.updateValue;
+
+            if (doc.tagCattegory != null) { tagCattegory = doc.tagCattegory; }
+            if (doc.lookupValue != null) { lookupValue = doc.lookupValue; }
+            if (doc.tagScore != null) { tagScore = doc.tagScore; }
+            if (doc.creationDate != null) { creationDate = doc.creationDate; }
+        }
+
+        //creationDate
+        if (doc.creationDate != null){
+            creationDate = doc.creationDate;
+        }
+
+        // return businessrule as object
+        businesRule.lookupValue = lookupValue;
+        businesRule.typeBusinessRule = typeBusinessRule;
+        businesRule.tagCattegory = tagCattegory;
+        businesRule.tagScore = tagScore;
+        businesRule.creationDate = creationDate;
+        businesRule.lastUpdatedate = lastUpdatedate;
+
+        console.log(businesRule);
+        return businesRule
+    }
+
+    var findBusinessRule = function (db, callback) {
+        var cursor = db.collection('businessrules').find({_id: ObjectId(req.body._id)});
+        cursor.each(function (err, doc) {
+            assert.equal(err, null);
+            if (doc != null) {
+                var updBusinessRule = setBusinessRule(doc);
+                console.log(updBusinessRule);
+
+                db.collection('businessrules').findAndModify(
+                    {_id: ObjectId(req.body._id)}, // query
+                    [['_id', 'asc']],  // sort order
+                    {$set: {lookupValue: updBusinessRule.lookupValue
+                          , typeBusinessRule: updBusinessRule.typeBusinessRule
+                          , tagCattegory: updBusinessRule.tagCattegory
+                    }}, // replacement, replaces only the field "hi"
+                    {}, // options
+                    function (err, object) {
+                        if (err) {
+                            console.warn(err.message);  // returns error if no matching object found
+                        } else {
+                            console.dir(object);
+                        }
+                    }
+                );
+            }
+        });
+    };
+
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+
+        findBusinessRule(db, function() {
+            db.close();
+        });
+    });
+
+    //res.redirect('/BusinessRules');
+
+    }
+
