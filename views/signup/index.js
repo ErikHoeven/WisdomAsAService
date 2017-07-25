49f09f5a9523dd78f8e44446a8630fcc80ce4b47
erik.hoeven@gmail.com <erik.hoeven@gmail.com>
2017-07-25 20:33:54 +0200

    BUG: Programmatuur gaat vanuit de Business Rule. Add niet terug naar de Business Rule overzicht pagina (Done)
    1. Dictionary toegevoegd aan Business Rule.
        A. Werkwoorden (Done)
        B. ZelfstandigNaamwoorden (In Done)
        C. Async:
           C.1 Ophalen Tweets (Done)
           C.2 Ophalen zelfstandignaamwoorden (Done)
           C.3 Vertraging in bouwen van het opbouwen van de Corpus (Done)
           C.4 Loop door tweets en (In Progress )
                  1. Nu met behulp van functies functionaliteiten opgebroken waardoor het makkelijker is
                      om het na te lezen! (Done)
                   2. GraphStructure wordt nu naar de Dahsboard pagina gestuurd ( Done)
                        Test voor 1 tweet
         D.1 Directed Graph in Dashboard (Done )
         D.2 samenvoegen van meerdere tweets en tonen in Dashboard (Done )
        ------------------------------------------------------------------------------------------------------------------------------------------
         E Test uitgevoerd met 40 tweets
          E.1 # wordt niet gevonden bij het zoeken naar cattegorie waarden.  (Done)
    F.1 Aantallen toevoegen bij nodes child (Done )
    F.2 Aantallen in lineare scale zetten (Done)
    F.3 Aantallen optellen bij parent (Done)
    F.4  Aantal optellen bij master (Done)
     ----------------------------------------------------------------------------------------------------------------------------------------------
     G Tweets tonen bij Node !! ( Done)
     G.1 Functie voor het filteren van Tweets  (Done)
       G.1.A Filteren op corpus en cattegorie (Done )
       G.1 B Filteren alleen op cattegorie (Done)
       G.1.C Filteren op alleen master (Done)
      G.2 Tweets tonen (net als twitter) in kolom (Done)
        G.2.A Profile Image (Done)
        G.2.A Photo voor tweet (Done)
        G.2.A Tweet text (Done)
        G.2.A Screen Name (Done)
      --------------------------------------------------------------------------------------------------------------------------------------------
     Sentiment:
     F1. Upload lijst met engelse sentiment woorden (Done)
     F2. Vertalen van Sentiment lijst (In Progress)
     F2.1 Vertaal scherm gemaakt op basis van tab seperated files (Done)
     F2.2 Vertaal scherm naar Database (Done)
     F3. NLTK toepassen  (To Do)
     F4. Bijvoegelijknaamwoorden ophalen (Done)
     F5. Bijvoegelijknaamwoorden detecteren in Tweet (Done)
     F6. Bijvoegelijknaamwoorden aggegreren tot aantal per Bijvoegelijknaamwoord ( Done )
     F7 Bijvoegelijknaamwoorden tonen in een tabel waarin de gebruiker kan aangeven welke score die kan meegeven aan de bijvoegelijknaamwoorden ( Done)
     F8. Bijvoegelijknaamwoorden top 25 gebruiken om tweets te taggen( Opbouw van trainingset) ( Done)
     F9. Opbouwen van Bayes Model (Done)
     F10. Sentiment tonen voor tweets (en nieuwe tweets) (To Do )
     F11. Generieke tabel op basis van JSON structuur (In Progress)
     F11.A Tonen in mooi format (done)
     F11.B Editable maken van tabel (done)
     F11.C Paging (done)
     F11.C1 Problems with passing through objects to function in combination with an event (done)
     F11.D Filter (In progress)
     F11.D1 TypeList filter (done)
     F11.D2 SearchLookup Filter (done)
     F11.D2.1 Auto Completion (done : basic scenario )
     ------------ H SIGN IN --------------------------------
     H01 Check if User exist in the database (done )
     H02 Upload avatar (done)
     H03 Show messages in screen (to do)

--- views/signup/index.js
+++ views/signup/index.js
@@ -21,7 +21,8 @@ var multer  =   require('multer'),
     br = db.get('businessrules'),
     url = 'mongodb://localhost:27017/commevents',
     sys = require('sys'),
-    businessRules = db.get('businessrules');
+    businessRules = db.get('businessrules'),
+    userWaas = db.get('user_waas')
 
 
 exports.init = function(req, res){
@@ -32,7 +33,7 @@ exports.fileupload = function (req, res, next){
     console.info('Upload File')
     testFolder =  './uploads'
     console.info('------------------------------------')
-    var name, email, username, password, confirmpassword
+    var frmUser = {}, locals = [], dbWaasUser, message = []
 
 
 
@@ -44,16 +45,67 @@ exports.fileupload = function (req, res, next){
         console.info('succes')
         console.info(req.body.name)
 
-        name = req.body.name
-        email = req.body.email
-        username = req.body.username
-        password = req.body.password
-        confirmpassword = req.body.confirm
+        frmUser.name = req.body.name
+        frmUser.email = req.body.email
+        frmUser.username = req.body.username
+        frmUser.password = req.body.password
+        frmUser.confirmpassword = req.body.confirm
 
+        mongo.connect(url, function (err, db) {
+            var tasks = [
+                // Load users
+                function (callback) {
+                    db.collection('user_waas').find({name: frmUser.name}).toArray(function (err, wUser) {
+                        if (err) return callback(err);
+                        locals.wUser = wUser;
+                        callback();
+                    })
+                }
+                // Load CSV from table
+                //function (callback) {
+                //    db.collection('csv').find({}).toArray(function (err, csv) {
+                //        if (err) return callback(err);
+                //        locals.csv = csv;
+                //        callback();
+                //    });
+                //},
+            ];
+            console.info('--------------- EINDE ASYNC ------------------------')
+            async.parallel(tasks, function (err) {
+                if (err) return next(err);
+                db.close();
+                dbWaasUser = locals.wUser
+                message = checkUserExist(dbWaasUser,frmUser)
+                if (message.length == 0 || !message){
+                    userWaas.insert(frmUser)
+                }
+                console.info(message)
+                //console.info(locals.csv)
+                //searchWords = translateSearchResult(selectedText, locals.csv)
+                //console.info('searchWords:')
+                //console.info(searchWords)
+                //restult = matchWordWithBusinessRules(searchWords,businessrules)
+                //console.info('-------------start restult:------------------')
+                //console.info(restult)
+                //console.info('-------------einde restult:------------------')
+                //res.status(201).json(restult)
+                res.render('signup/index')
 
-
-
-        res.render('signup/index')
-
+            })
+        })
     });
+}
+
+function checkUserExist(objdbUser,frmUser) {
+    var message = []
+    if(objdbUser[0].name == frmUser.name){
+        message.push('Username already exist')
+    }
+    if (objdbUser[0].email == frmUser.email){
+        message.push('email already exist')
+    }
+    if (objdbUser[0].username == frmUser.username){
+        message.push('username already exist')
+    }
+    return message
 }
